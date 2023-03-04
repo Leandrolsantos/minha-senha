1fbc64031cdfe2b688303ec4af379a9f7fc76cd6
Leandro Leme dos Santos <leandrolemesantos@gmail.com>
2023-03-04 18:13:20 -0300

    61º dia de CTD - Avançando no JavaScript

--- assets/js/scrip.js
+++ assets/js/scrip.js
@@ -9,3 +9,20 @@ let containerPassword = document.querySelector("#container-password");
 
 let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#";
 let novaSenha = "";
+
+sizePassword.innerHTML = sliderElement.value;
+
+sliderElement.oninput = function(){
+   sizePassword.innerHTML = this.value; 
+}
+
+function generatePassword(){
+    
+    let pass = "";
+    for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
+      pass += charset.charAt(Math.floor(Math.random() * n)); 
+    }
+
+    console.log(pass); 
+
+}
